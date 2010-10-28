8f4f12b81fa60bb4ba0e8888005c8a6d433ee042
david@wimsey.us <david@wimsey.us@e7245d35-3a27-1693-5822-4d4017ff1c13>
2010-10-28 02:54:27 +0000

    Basic viewport which displays all sensor values in a list view

---
+++ client/winforms/SensorListViewPort.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace FishnSpots
+{
+	partial class SensorListViewPort
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.sensorListView = new System.Windows.Forms.ListView();
+			this.colSensorName = new System.Windows.Forms.ColumnHeader();
+			this.colSensorType = new System.Windows.Forms.ColumnHeader();
+			this.colSensorValue = new System.Windows.Forms.ColumnHeader();
+			this.colSensorScale = new System.Windows.Forms.ColumnHeader();
+			this.SuspendLayout();
+			// 
+			// sensorListView
+			// 
+			this.sensorListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.sensorListView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.sensorListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colSensorName,
+            this.colSensorType,
+            this.colSensorValue,
+            this.colSensorScale});
+			this.sensorListView.GridLines = true;
+			this.sensorListView.Location = new System.Drawing.Point(0, 0);
+			this.sensorListView.Name = "sensorListView";
+			this.sensorListView.Size = new System.Drawing.Size(507, 322);
+			this.sensorListView.TabIndex = 0;
+			this.sensorListView.UseCompatibleStateImageBehavior = false;
+			this.sensorListView.View = System.Windows.Forms.View.Details;
+			// 
+			// colSensorName
+			// 
+			this.colSensorName.Text = "Name";
+			this.colSensorName.Width = 164;
+			// 
+			// colSensorType
+			// 
+			this.colSensorType.Text = "Type";
+			this.colSensorType.Width = 74;
+			// 
+			// colSensorValue
+			// 
+			this.colSensorValue.Text = "Value";
+			this.colSensorValue.Width = 188;
+			// 
+			// colSensorScale
+			// 
+			this.colSensorScale.Text = "Scale";
+			// 
+			// SensorListViewPort
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.sensorListView);
+			this.Name = "SensorListViewPort";
+			this.Size = new System.Drawing.Size(507, 322);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListView sensorListView;
+		private System.Windows.Forms.ColumnHeader colSensorName;
+		private System.Windows.Forms.ColumnHeader colSensorValue;
+		private System.Windows.Forms.ColumnHeader colSensorType;
+		private System.Windows.Forms.ColumnHeader colSensorScale;
+
+	}
+}
---
+++ client/winforms/SensorListViewPort.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FishnSpots
+{
+	public partial class SensorListViewPort : FSViewPort
+	{
+		public SensorListViewPort()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// FSEngine this view port uses
+		/// </summary>
+		public FSEngine engine
+		{
+			get
+			{
+				return fsEngine;
+			}
+			set
+			{
+				if(this.fsEngine != null) {
+					// clear any existing hooks
+					foreach(KeyValuePair<string, SensorValue> kvp in fsEngine.Sensors) {
+						kvp.Value.OnValueUpdated -= sv_OnValueUpdated;
+					}
+
+					// clear the item list
+					this.sensorListView.Items.Clear();
+				}
+
+				this.fsEngine = value;
+				string[] rowItems;
+				// now add our sensors to the list
+				SensorValue sv;
+				foreach(KeyValuePair<string,SensorValue> kvp in fsEngine.Sensors) {
+					sv = kvp.Value;
+
+					rowItems = new string[4];
+					rowItems[0] = kvp.Key;
+					rowItems[1] = sv.m_SensorType.ToString();
+					rowItems[2] = sv.Value.ToString();
+					rowItems[3] = "M";
+					//lvi = new ListViewItem(rowItems);
+					this.sensorListView.Items.Add(new ListViewItem(rowItems));
+					sv.OnValueUpdated += sv_OnValueUpdated;
+				}
+			}
+		}
+
+		private IAsyncResult r = null;
+		void sv_OnValueUpdated(SensorValue sender, object oldValue)
+		{
+			try {
+				if(this.InvokeRequired) {
+					// this makes sure we don't pile up, we'll wait for the last one to complete, since we're
+					// just updating the display
+					if(r != null) {
+						if(!r.IsCompleted) {
+							int p_GUIWaitTime = (int)(fsEngine.prefs.GUI.AsyncWaitTimeout/1000);
+							if(!r.AsyncWaitHandle.WaitOne((int)(fsEngine.prefs.GUI.AsyncWaitTimeout/1000))) {
+								// previous invoke did not complete in time
+								// skip updating the gui until it does!
+							}
+						}
+					}
+					r = this.BeginInvoke(new SensorValue.SensorValueUpdated(this.sv_OnValueUpdated), sender, oldValue);
+					//this.Invoke(new SensorValue.SensorValueUpdated(this.PositionUpdated), sender, oldValue);
+					return;
+				}
+			} catch(Exception ex) {
+				throw ex;
+			}
+
+			int i = 0;
+			ListViewItem lvi = null;
+			foreach(ListViewItem li in this.sensorListView.Items) {
+				if(!string.IsNullOrEmpty(li.Text)) {
+					if(li.Text.Equals(sender.Name)) {
+						lvi = li;
+					}
+				}
+			}
+			if(lvi == null) {
+				// couldn't find any list item named for this sensor
+				i++;
+			}
+			if(lvi.SubItems.Count == 4) {
+				try {
+					if(sender.Value != null) {
+						lvi.SubItems[2].Text = sender.Value.ToString();
+					} else {
+						lvi.SubItems[2].Text = "(NULL)";
+					}
+				} catch(Exception ex) {
+					i++;
+				}
+			} else {
+				i++;
+			}
+		}
+	}
+}
