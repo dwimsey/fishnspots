5c8fe54e967c92c5696916ce3ffe089ee289068a
david@wimsey.us <david@wimsey.us@e7245d35-3a27-1693-5822-4d4017ff1c13>
2010-11-19 23:12:40 +0000

    Adding missing compass source file

---
+++ trunk/client/winforms/GLCompassViewPort.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK.Graphics.OpenGL;
+
+namespace FishnSpots
+{
+	public class GLCompassViewPort : GLViewPortBase
+	{
+		public GLCompassViewPort()
+		{
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+			glControl.CreateControl();
+			glControl.MakeCurrent();
+			GL.ClearColor(System.Drawing.SystemColors.Control);
+			GL.Clear(ClearBufferMask.AccumBufferBit|ClearBufferMask.ColorBufferBit|ClearBufferMask.DepthBufferBit|ClearBufferMask.StencilBufferBit);
+			glControl.SwapBuffers();
+		}
+	}
+}
